8935fc7b99081b528293ad158dd77b7ff1ee338d
digitalbussiness <digitalbussiness331@gmail.com>
2025-05-21 21:47:53 -0300

    Create App.js

---
+++ public/src/src/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-3xl font-bold">Bem-vindo ao ReplyFlow AI</h1>
+      <p>Aplicação React + Firebase + Tailwind configurada!</p>
+    </div>
+  );
+}
+
+export default App;
